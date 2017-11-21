a0ba886a7150d1953a132cfa2cc5b48e1420d560
Daniel Hernandez <danielhp95@gmail.com>
2017-11-21 12:50:33 +0000

    refactored install script, added replys (currently  commented out)

--- install_sc2le.py
+++ install_sc2le.py
@@ -8,26 +8,93 @@ from bs4 import BeautifulSoup
 logging.basicConfig(level=logging.INFO)
 
 unzip_password = 'iagreetotheeula'
+blizzard_repo = 'https://github.com/Blizzard/s2client-proto#downloads'
+
+
+def download_single_link(link):
+    return requests.get(link)
+
+
+def download_multiple_links(links):
+    total_number_of_links = len(links)
+    zip_files_directories = []
+    for number, link in enumerate(links):
+        logging.info('Downloading: {} {}/{}'.format(link, number+1, total_number_of_links))
+        response  = download_single_link(link)
+        file_name = str(number) + 'temp.zip'
+        file_path = save_binary_to_file(response.content, file_name)
+        zip_files_directories.append(file_path)
+    return zip_files_directories
+
+
+def save_binary_to_file(binary_content, file_name):
+    with open(file_name, 'wb') as f:
+        f.write(binary_content)
+        temporary_file_path = os.path.abspath(f.name)
+    return temporary_file_path
+
+
+def extract_multiple_zip_files_into_directory(zips, directory):
+    logging.info('Extracting files')
+    for zip_file in zips:
+        extract_zip_file_into_directory(zip_file, directory)
+    pass
+
+
+def extract_zip_file_into_directory(zip_file, directory):
+    logging.info('Extracting {} into {}'.format(zip_file, directory))
+    success = call(['unzip', '-P', unzip_password, zip_file, '-d', directory])
+    return success
+
+
+def remove_temporay_files(temporary_files):
+    for zip_file in temporary_files:
+        os.remove(zip_file)
 
 
 def check_if_startcraftII_exists():
     environment_variable_exists = 'SC2PATH' in os.environ
-    root_SC2_directory_exists   = os.path.isdir('{}/{}'.format(os.getenv("HOME"),'StarCraftII'))
+    root_SC2_directory_exists   = os.path.isdir('{}/{}'.format(os.getenv("HOME"), 'StarCraftII'))
     return environment_variable_exists or root_SC2_directory_exists
+
+
+def get_starcraft_directory():
+    return os.getenv('SC2PATH') if 'SC2PATH' in os.environ else os.getenv('HOME')
+
+
+def parse_blizzard_starcraftII_repository():
+    response = requests.get(blizzard_repo)
+    return BeautifulSoup(response.content, 'lxml')
 
 
 def find_latest_starcraftII_link(soup):
     linux_packages_tag  = soup.find('a', href='#linux-packages')
     all_distributions   = linux_packages_tag.parent.findNextSibling()
-    latest_distribution = all_distributions.findChildren()[0].findChild() 
+    latest_distribution = all_distributions.findChildren()[0].findChild()
     download_link = latest_distribution.attrs['href']
     return download_link
+
+
+def find_all_map_links(soup):
+    map_packs_tag       = soup.find('a', href='#map-packs')
+    all_map_packs_list  = map_packs_tag.parent.findNextSibling()
+    clean_all_map_packs_list = [list_item.findChild() for list_item in all_map_packs_list.findChildren() if list_item.findChild() is not None]
+    all_maps  = [map_tag.attrs['href'] for map_tag in clean_all_map_packs_list]
+    return all_maps
+
+
+def find_all_replay_links(soup):
+    replay_packs_tag = soup.find('a', href='#replay-packs')
+    all_replay_packs_list = replay_packs_tag.parent.findNextSibling()
+    clean_all_replay_packs_list = [list_item.findChild() for list_item in all_replay_packs_list.findChildren()]
+    clean_all_replay_packs_list = list(filter(lambda x: x is not None, clean_all_replay_packs_list))
+    all_replays = [replay_tag.attrs['href'] for replay_tag in clean_all_replay_packs_list]
+    return all_replays
 
 
 def download_latest_starcraftII():
     logging.info('Downloading Starcraft II (linux)')
     logging.info('This will take a while')
-    blizzard_repo = 'https://github.com/Blizzard/s2client-proto#downloads'
     response = requests.get(blizzard_repo)
     soup = BeautifulSoup(response.content, 'lxml')
 
@@ -37,99 +104,63 @@ def download_latest_starcraftII():
     logging.info('Size of StarcraftII distribution: {}'.format(len(request.content)))
     with open('StarcraftII.zip', 'wb') as f:
         for chunk in request.iter_content(chunk_size=4096):
-            if chunk: 
+            if chunk:
                 f.write(chunk)
         starcraft_zip = os.path.abspath(f.name)
     logging.info('Starcraft II finished downloading')
     return starcraft_zip
 
 
-def extract_starcraftII(starcraft_zip):
-    logging.info('Extracting Starcraft II in directory {}'.format(get_starcraft_directory()))
-    extract_zip_file_into_directory(starcraft_zip, get_starcraft_directory())
-
-
-def find_all_map_links(soup):
-    map_packs_tag       = soup.find('a', href='#map-packs')
-    all_map_packs_list  = map_packs_tag.parent.findNextSibling()
-    clean_all_map_packs_list = [ list_item.findChild() for list_item in all_map_packs_list.findChildren() if list_item.findChild() is not None]
-    all_maps  = [map_tag.attrs['href'] for map_tag in clean_all_map_packs_list]
-    return all_maps
-
-def find_all_replay_links(soup):
-    replay_packs_tag = soup.find('a', href='#replay-packs')
-    all_replay_packs_list = replay_packs_tag.parent.findNextSibling()
-    clean_all_replay_packs_list = [ list_item.findChild() for list_item in all_replay_packs_list.findChildren() if list_item.findChild is not None]
-    all_replays = [replay_tag.attres['href'] for replay_tag in clean_all_replay_packs_list]
-    return all_replays
-
-
-# test
 def download_maps():
     logging.info('Downloading all available MAPS')
-    blizzard_repo = 'https://github.com/Blizzard/s2client-proto#downloads'
-    response = requests.get(blizzard_repo)
-    soup = BeautifulSoup(response.content, 'lxml')
-
+    soup = parse_blizzard_starcraftII_repository()
     download_links = find_all_map_links(soup)
-    number_of_map_sets = len(download_links)
-    logging.info('Found {} available sets of MAPS'.format(number_of_map_sets))
-
-    map_zips = []
-    for number, map_link in enumerate(download_links):
-        logging.info('Downloading: {} {}/{}'.format(map_link, number+1, number_of_map_sets))
-        map_zip_binary = requests.get(map_link)
-        with open(str(number) + 'temp.zip','wb') as f:
-            f.write(map_zip_binary.content)
-            map_zips.append(os.path.abspath(f.name))
-
-    return map_zips
+    logging.info('Found {} available MAPS'.format(len(download_links)))
+    maps_zips = download_multiple_links(download_links)
+    return maps_zips
 
 
 def download_replays():
     logging.info('Downloading all available REPLAYS')
-    blizzard_repo = 'https://github.com/Blizzard/s2client-proto#downloads'
-    response = requests.get(blizzard_repo)
-    soup = BeautifulSoup(response.content, 'lxml')
-
+    soup = parse_blizzard_starcraftII_repository()
     download_links = find_all_replay_links(soup)
-    number_of_replay_packs = len(download_links)
-    logging.info('Found {} available sets of REPLAYS'.format(number_of_replay_packs))
+    logging.info('Found {} available sets of REPLAYS'.format(len(download_links)))
+    replay_pack_zips = download_multiple_links(download_links)
+    return replay_pack_zips
 
 
-def extract_maps(map_zips):
-    logging.info('Extracting maps')
-    for map_zip in map_zips:
-        logging.info('Extracting {} into Starcraft Maps Folder'.format(map_zip))
-        extract_zip_file_into_directory(map_zip, get_starcraft_directory() + '/StarCraftII/Maps/')
-    pass
+def download_and_extract_starcraftII():
+    logging.info('Downloading StarcraftII from https://github.com/Blizzard/s2client-proto#downloads')
+    starcraft_zip = download_latest_starcraftII()
+    extract_zip_file_into_directory(starcraft_zip, get_starcraft_directory())
+    os.remove(starcraft_zip)
 
 
-def extract_zip_file_into_directory(zip_file, directory):
-    success = call(['unzip','-P', unzip_password, zip_file,'-d',directory])
-    return success
-
-
-def get_starcraft_directory():
-    return os.getenv('SC2PATH') if 'SC2PATH' in os.environ else os.getenv('HOME')
-
-
-# Main
-def install_sc2le(starcraft_directory=os.getenv('Home')):
-    starcraft_exists = check_if_startcraftII_exists()
-    logging.info('StarcraftII is {}'.format('PRESENT' if starcraft_exists else 'MISSING'))
-    if not starcraft_exists:
-        logging.info('Downloading StarcraftII from https://github.com/Blizzard/s2client-proto#downloads')
-        starcraft_zip = download_latest_starcraftII()
-        extract_starcraftII(starcraft_zip)
-        os.remove(starcraft_zip)
-
+def download_and_extract_maps():
     map_zips = download_maps()
-    extract_maps(map_zips)
-    for zip_file in map_zips:
-        os.remove(zip_file)
+    maps_directory = get_starcraft_directory() + '/StarCraftII/Maps/'
+    extract_multiple_zip_files_into_directory(map_zips, maps_directory)
+    remove_temporay_files(map_zips)
 
-    # Add replays as well?
+
+def download_and_extract_replays():
     replay_zips = download_replays()
+    replays_directory = get_starcraft_directory() + '/StarCraftII/Replays/'
+    extract_multiple_zip_files_into_directory(replay_zips, replays_directory)
+    remove_temporay_files(replay_zips)
+
+
+def install_sc2le(starcraft_directory=os.getenv('Home')):
+    # starcraft_exists = check_if_startcraftII_exists()
+    # logging.info('StarcraftII is {}'.format('PRESENT' if starcraft_exists else 'MISSING'))
+    # if not starcraft_exists:
+    #     download_and_extract_starcraftII()
+
+    download_and_extract_maps()
+    # download_and_extract_replays()
+
 
 install_sc2le()
+# print(download_replays())
+# print(get_starcraft_directory())
+# extract_zip_file_into_directory('/home/sarios/Projects/sc2l-setup/0temp.zip', get_starcraft_directory() + '/Replays/')
